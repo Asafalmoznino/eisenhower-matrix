103eb05f18c4a29721330dc89b1c6d35fbc85c41
Asafalmoznino <asafalmoznino@gmail.com>
2025-02-22 18:50:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("❌ Error: Root element not found!");
+}
