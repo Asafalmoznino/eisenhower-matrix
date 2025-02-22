a1faef7fa091726b41b567911e43a4bd28c19f06
Asafalmoznino <asafalmoznino@gmail.com>
2025-02-22 16:29:40 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EisenhowerMatrix from "./EisenhowerMatrix";
+
+function App() {
+  return (
+    <div>
+      <EisenhowerMatrix />
+    </div>
+  );
+}
+
+export default App;
